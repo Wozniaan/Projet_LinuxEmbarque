21d5c1ea9c50f1e62cc9593b6590855be99c0df6
LouisValery <louis.valery@ensta-bretagne.org>
2019-02-11 10:21:57 +0100

    maj serveur_sm

--- servoMoteur/serveur_sm.py
+++ servoMoteur/serveur_sm.py
@@ -1,10 +1,10 @@
 
-"""# -*- coding: utf-8 -*-            
-"""                              
+# -*- coding: utf-8 -*-            
+"""
 Created on Tue Jan 29 09:51:44 2019                        
                                                          
 @author: louis                                             
-                                                   
+"""                                                   
                                                            
 import RPi.GPIO as GPIO                                  
 import time                                                
@@ -23,8 +23,11 @@ pwm=GPIO.PWM(17,100)
 pwm.start(5)                                             
                                                                   
 pwm.ChangeDutyCycle(angle_init)                                        
-time.sleep(2)                                                     
-     "
+time.sleep(2) 
+
+                                                    
+hote = ''
+port = 12800
 
 print"\n+----------/ ServoMoteur  Controlleur /----------+"
 print"|                                                |"
@@ -35,27 +38,29 @@ print"+------------------------------------------------+\n"
 print"Comment controler le Servo ?"
              
                          
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('', 15555))         
-                       
+socket_sm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_sm.bind((hote, port))         
+socket_sm.listen(5) 
+client, adress = socket_sm.accept()                    
+print "{} connected".format(adresse) 
+
+                                 
 while True:             
-#    print "serveur microcontroller listening"             
-#    socket.listen(5)                                    
-#    client, adress = socket.accept()                    
-#    print "{} connected".format(adresse)               
+    print "serveur microcontroller listening"            
                                                            
-#    choix = client.recv(255)                              
-#    if (int(choix) != 1 or int(choix) != 2 ):           
-#        print "ERROR: expected 1 or 2, received {}".format(choix)
-    choix = int(input("1. aller à gauche\n2. pour aller à droite\n"))
+    choix = client.recv(255) 
+                             
+    if (int(choix) != 1 or int(choix) != 2 ):           
+        print "ERROR: expected 1 or 2, received {}".format(choix)
+#    choix = int(input("1. aller à gauche\n2. pour aller à droite\n"))
                                                            
     if (choix == 1) :                                      
-        angle = -5                                       
+        delta_angle = -5                                       
     elif(choix == 2):                                      
-        angle = 5                                        
+        delta_angle = 5                                        
                                                            
     duree = 0                                            
-    angleChoisi = (current_angle + angle)/10 + 5  #increment de 5 degré à chaque fois
+    angleChoisi = (current_angle + delta_angle)/10 + 5  #increment de 5 degré à chaque fois
            
     if angleChoisi <= 6: #evite servomoteur en butee
         angleChoisi = 6                                   
@@ -66,5 +71,8 @@ while True:
     pwm.ChangeDutyCycle(angleChoisi)          
     time.sleep(duree)                                             
                                                                        
-    current_angle += angle           
+    current_angle += angle 
+          
 GPIO.cleanup() 
+socket_sm.close()
+client.close()
